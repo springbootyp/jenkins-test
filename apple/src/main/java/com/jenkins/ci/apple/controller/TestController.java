ea7c3c127da27e1498454b58e17908f4b9c440fa
jason <15673629224@163.com>
2019-07-02 16:47:08 +0800

    add

--- apple/src/main/java/com/jenkins/ci/apple/controller/TestController.java
+++ apple/src/main/java/com/jenkins/ci/apple/controller/TestController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TestController {
 
-    String falg = "Jenkins ....  test";
+    String falg = "Jenkins ....  test  111 ";
 
     @RequestMapping(value = "/index")
     public String index(){
